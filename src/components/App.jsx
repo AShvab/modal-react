5abdafa6c2e2ce53c3378d8189cf30bc49d574b2
AShvab <shvabovska@gmail.com>
2023-05-11 20:45:52 +0300

    add modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Modal from './Modal';
+
+class App extends Component {
+  state = {
+    showModal: false,
+  };
+  componentDidMount() {
+    // console.log('Mount App Component');
+  }
+
+  componentDidUpdate() {
+    // console.log('Update App Component');
+  }
+
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
+
+  render() {
+    const { showModal } = this.state;
+
+    return (
+      <div>
+        <button type="button" onClick={this.toggleModal}>
+          Open Modal
+        </button>
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <h1>Modal Title</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem
+              veritatis accusamus fuga illo, vel deserunt dolorum nihil ut
+              ratione ipsa perspiciatis, in alias blanditiis iure? Voluptate
+              alias eos repudiandae beatae!
+            </p>
+            <button type="button" onClick={this.toggleModal}>
+              Close Modal
+            </button>
+          </Modal>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+import css from './Modal.module.css';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export default class Modal extends Component {
+  componentDidMount() {
+    // console.log('Mount Modal Component');
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    // console.log('Unmount Modal Component');
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      this.props.onClose();
+    }
+  }
+
+  render() {
+    return createPortal(
+      <div className={css.Modal__backdrop} onClick={this.handleBackdropClick}>
+        <div className={css.Modal__content}>{this.props.children}</div>
+      </div>,
+      modalRoot
+    );
+  }
+}
